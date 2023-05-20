b01673547c9ca1ee1057cc7f4442dfbb78c73734
Priyabrata Maji <priyabratamaji139@gmail.com>
2023-05-20 17:29:56 +0530

    Added airrvalTime>depertureTime validation

--- src/middlewares/flight-middlewares.js
+++ src/middlewares/flight-middlewares.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require('http-status-codes');
 
 const { ErrorResponse } = require('../utils/common');
 const AppError = require('../utils/errors/app-error');
+const { compareTime } = require('../utils/helpers/datetime-helpers');
 
 function validateCreateRequest(req, res, next) {
     if(!req.body.flightNumber) {
@@ -59,6 +60,13 @@ function validateCreateRequest(req, res, next) {
         return res
                 .status(StatusCodes.BAD_REQUEST)
                 .json(ErrorResponse);
+    }
+    if ( !compareTime(req.body.arrivalTime,req.body.departureTime) ) {
+        ErrorResponse.message = 'Something went wrong while creating flight';
+        ErrorResponse.error = new AppError(['Arrival-Time can not be equal or less than departure-Time'], StatusCodes.BAD_REQUEST);
+        return res
+                .status(StatusCodes.BAD_REQUEST)
+                .json(ErrorResponse); 
     }
     next();
 }
